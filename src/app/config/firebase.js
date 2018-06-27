e58d7347739cd9ec0562b8aa9766a0926d806177
Bill Lennon <william.lennon@nordstrom.com>
2018-06-27 10:19:48 -0600

    new apikey

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 import 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDTz7PhEojER4iyERzBNm32nz0pwKM3qDA",
-  authDomain: "revents-1077d.firebaseapp.com",
-  databaseURL: "https://revents-1077d.firebaseio.com",
-  projectId: "revents-1077d",
-  storageBucket: "revents-1077d.appspot.com",
-  messagingSenderId: "631636745857"
+const firebaseConfig ={
+  apiKey: "AIzaSyAXOQ29WnxmUXsddE-8Ax2AOWYOYJpYcWI",
+  authDomain: "revents-207802.firebaseapp.com",
+  databaseURL: "https://revents-207802.firebaseio.com",
+  projectId: "revents-207802",
+  storageBucket: "",
+  messagingSenderId: "647910020761"
 }
 
 firebase.initializeApp(firebaseConfig);
